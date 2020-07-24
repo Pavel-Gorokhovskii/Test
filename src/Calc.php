932029e0b5a06a88a658a139d9714a5644bcd59e
Pavel-Gorokhovskii <Pavel-Gorokhovskii@yandex.ru>
2020-07-24 20:07:51 +0300

    Summary

---
+++ src/Calc.php
@@ -0,0 +1,27 @@
+<?php
+class Calculator
+{
+    private float $a;
+    private float $b;
+    public function __construct(float $a, float $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+    public function sum(): float
+    {
+        return $this->a + $this->b;
+    }
+    public function mul(): float
+    {
+        return $this->a * $this->b;
+    }
+    public function dif(): float
+    {
+        return $this->a - $this->b;
+    }
+    public function div(): float
+    {
+        return $this->a / $this->b;
+    }
+}
